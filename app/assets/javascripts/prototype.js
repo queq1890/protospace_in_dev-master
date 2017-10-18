44e6d3ef54a2a642ba47f7c176fd355b0bb6e3e1
queq1890 <yujimatsumoto@matsumotohiroshinoMacBook-Pro.local>
2017-10-18 09:35:54 +0900

    Add ids to js

--- app/assets/javascripts/prototype.js
+++ app/assets/javascripts/prototype.js
@@ -1,8 +1,8 @@
 $(function(){
-  $('form').on('change', 'input[type="file"]', function(e) {
+  $('form').on('change', 'input[type="file"][id="main_file"]', function(e) {
     var file = e.target.files[0],
         reader = new FileReader(),
-        $preview = $(".preview");
+        $preview = $(".preview_main");
         t = this;
 
     if(file.type.indexOf("image") < 0){
@@ -13,6 +13,28 @@ $(function(){
       return function(e) {
         $preview.empty();
         $preview.append($('<img>').attr({
+                  src: e.target.result,
+                  class: "preview",
+                  title: file.name
+              }));
+      };
+    })(file);
+
+    reader.readAsDataURL(file);
+  });
+  $('#sub_file').on('change', function(e) {
+    var file = e.target.files[0],
+        reader = new FileReader(),
+        t = this;
+
+    if(file.type.indexOf("image") < 0){
+      return false;
+    }
+
+    reader.onload = (function(file) {
+      return function(e) {
+        $(this).parent().empty();
+        $(this).parent().append($('<img>').attr({
                   src: e.target.result,
                   class: "preview",
                   title: file.name
