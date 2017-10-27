2e862ff1af10962e8374d8545fe4385cdd2d059d
岩永規秀 <motohide@iwanagatadashishuus-MacBook-Pro.local>
2017-10-27 11:10:52 +0900

    Create-controller
    Like機能に必要な、コントローラーを作成。
    ルーティングに記述。

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :prototypes do
+    resources :likes, only: [:create, :destroy]
+  end
   devise_for :users
   root 'prototypes#index'
   resources :prototypes, only: [:index, :new, :create, :show, :edit, :update, :destroy]
