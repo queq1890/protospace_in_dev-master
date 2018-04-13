57848cc3c98cc130cb17fe7590f8838828fe5c8f
queq1890 <mtmtatmat4@toki.waseda.jp>
2018-04-13 15:04:04 +0900

    デプロイ時にsecrets.ymlをアップロードするようにした

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,4 +21,16 @@ namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
+
+  desc 'Upload secrtes.yml'
+  task :upload do
+    on roles(:app) do |host|
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
+      end
+      upload!('config/database.yml', "#{shared_path}/config/secrets.yml")
+    end
+  end
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
 end
