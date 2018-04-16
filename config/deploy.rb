f51f20092a189a6b853f9a8c4bee34aa9a68a513
queq1890 <mtmtatmat4@toki.waseda.jp>
2018-04-16 10:25:02 +0900

    Fix typo

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,7 +30,7 @@ namespace :deploy do
     invoke 'unicorn:restart'
   end
 
-  desc 'Upload secrtes.yml'
+  desc 'Upload secrets.yml'
   task :upload do
     on roles(:app) do |host|
       if test "[ ! -d #{shared_path}/config ]"
